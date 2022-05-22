e9a352b7dff4a8e4c9a80538e6902ca85f14288c
Aki <a706394163@gmail.com>
2022-05-23 00:40:39 +0800

    lined render

--- main.go
+++ main.go
@@ -10,13 +10,29 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 const (
-	fontSize   = 48
-	fontPath   = "font/SourceHanSansCN-Regular.otf"
-	outputPath = "out/out.png"
+	fontSize    = 24
+	fontPath    = "font/SourceHanSansCN-Light.otf"
+	outputPath  = "out/out.png"
+	lineSpacing = 0
 )
+
+func measureString() (int, int) {
+
+	return 0, 0
+}
+
+type textLine struct {
+	text string
+}
+
+type TextDrawer struct {
+	lines []textLine
+}
 
 func loadFontFace() font.Face {
 	file, err := ioutil.ReadFile(fontPath)
@@ -38,26 +54,34 @@ func loadFontFace() font.Face {
 	return face
 }
 
-func addLabel(img *image.RGBA, x, y int, label string) {
+func addLabel(img *image.RGBA, x int, label string) {
 	col := color.RGBA{R: 0, G: 0, B: 0, A: 255}
-	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
 
 	d := &font.Drawer{
 		Dst:  img,
 		Src:  image.NewUniform(col),
 		Face: loadFontFace(),
-		Dot:  point,
 	}
-	for _, i := range label {
-		s := string(i)
-		bounds, _ := d.BoundString(s)
-		log.Println(s, "height:", bounds.Max.Y.Ceil()-bounds.Min.Y.Floor(), "width:", bounds.Max.X.Ceil()-bounds.Min.X.Floor())
+	runes := []rune(label)
+	last := 0
+	row := 1
+	for i := range runes {
+		if d.MeasureString(string(runes[last:i])).Round() > 800 {
+			d.Dot = fixed.Point26_6{X: fixed.I(x), Y: fixed.I(fontSize*row + (row-1)*lineSpacing)}
+			d.DrawString(string(runes[last : i-1]))
+			last = i - 1
+			row += 1
+		}
 	}
-	d.DrawString(label)
+	if left := runes[last:]; len(left) > 0 {
+		d.Dot = fixed.Point26_6{X: fixed.I(x), Y: fixed.I(fontSize*row + (row-1)*lineSpacing)}
+		d.DrawString(string(left))
+	}
 }
 
 func main() {
-	img := image.NewRGBA(image.Rect(0, 0, 500, 500))
+	start := time.Now()
+	img := image.NewRGBA(image.Rect(0, 0, 800, 1000))
 	for x := 0; x < img.Rect.Max.X; x++ {
 		for y := 0; y < img.Rect.Max.Y; y++ {
 			img.Set(x, y, color.RGBA{
@@ -68,7 +92,15 @@ func main() {
 			})
 		}
 	}
-	addLabel(img, 0, fontSize, "Hello Go 那没事了")
+	ss, err := ioutil.ReadFile("text.txt")
+	if err != nil {
+		panic(err)
+	}
+	addLabel(img, 0, strings.NewReplacer(
+		"\n", "",
+		"\t", "",
+		"\r", "",
+	).Replace(string(ss)))
 
 	f, err := os.Create(outputPath)
 	if err != nil {
@@ -78,4 +110,5 @@ func main() {
 	if err = png.Encode(f, img); err != nil {
 		panic(err)
 	}
+	log.Println("cost", time.Now().UnixMilli()-start.UnixMilli(), "ms")
 }
